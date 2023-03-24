f1df0a3a08dbc79daa8631ce5b171e063acd4767
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2023-03-25 00:52:47 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: unused_import, avoid_web_libraries_in_flutter
+
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
